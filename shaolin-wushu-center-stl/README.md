55fa695c2791cd50f52d9b3637ad841713b701a3
Myles M <myles.e.mayo@gmail.com>
2021-11-06 08:02:06 -0500

    setup

--- shaolin-wushu-center-stl/README.md
+++ shaolin-wushu-center-stl/README.md
@@ -1,4 +1,4 @@
-# ShaolinWushuCenter
+# ShaolinWushuCenterStl
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.1.
 
--- shaolin-wushu-center-stl/karma.conf.js
+++ shaolin-wushu-center-stl/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/shaolin-wushu-center'),
+      dir: require('path').join(__dirname, './coverage/shaolin-wushu-center-stl'),
       subdir: '.',
       reporters: [
         { type: 'html' },
